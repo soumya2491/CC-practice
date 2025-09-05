33cb0ab05ce9ad3f1fd25a05bc601a6264798037
soumya2491 <soumyachopra901@gmail.com>
2025-09-05 22:28:58 +0530

    practicing classes

---
+++ classesprac.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+class Student{
+    private :
+        std::string name;
+        int age;
+    public :
+        Student(){
+            std::cout <<"soumya";
+        }
+};
+int main(void){
+    Student s;
+}
